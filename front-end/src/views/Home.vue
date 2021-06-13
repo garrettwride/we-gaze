4fec27e03168555498c49e04a1508b4e92546da6
Garrett Wride <garrett.wride@gmail.com>
2021-06-13 10:03:23 -0400

    home

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+<div class="home">
+  <MyParties v-if="user" />
+  <Intro v-else />
+</div>
 </template>
 
 <script>
-
+import MyPhotos from '@/components/MyParties.vue';
+import Login from '@/components/Intro.vue';
+import axios from 'axios';
 export default {
   name: 'Home',
   components: {
-    
+    MyParties,
+    Intro,
+  },
+  async created() {
+    try {
+      let response = await axios.get('/api/users');
+      this.$root.$data.user = response.data.user;
+    } catch (error) {
+      this.$root.$data.user = null;
+    }
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    }
   }
 }
 </script>
